4fa8263980eac5aa226ebfa3ff0083fe57e0550b
Jochum Börger <jborger@xebia.com>
2017-06-16 16:44:07 +0200

    Upto start and line feature

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -6,7 +6,6 @@ var myHooks = function () {
   this.Before(function (scenario) {
     this.start = require("../../src/modules/start");
     this.followline = require("../../src/modules/followline");
-    this.finish = require("../../src/modules/finish");
 
     //constructing robot
     this.robot = new Robot(this.board);
@@ -15,7 +14,6 @@ var myHooks = function () {
     this.goForwardSpy = this.sandbox.spy(this.robot, "goForward");
     this.goLeftSpy = this.sandbox.spy(this.robot, "goLeft");
     this.goRightSpy = this.sandbox.spy(this.robot, "goRight");
-    this.stopSpy = this.sandbox.spy(this.robot, "stop");
   });
 
   this.After(function (scenario) {
--- src/app.js
+++ src/app.js
@@ -4,12 +4,10 @@ var board = new five.Board({port: process.argv[2]});
 var Robot = require("./robot");
 var startbutton = require("./modules/start");
 var followline = require("./modules/followline");
-var finish = require("./modules/finish");
 
 board.on("ready", function(){
   this.robot = new Robot(board);
   this.robot.initiateLeds();
   startbutton.initialize(this.robot);
   followline.initialize(this.robot);
-  finish.initialize(this.robot);
 });
